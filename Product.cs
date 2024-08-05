2f8ca1f5024fb19169018e8481dad88751166097
RandAlFaouri <rand.alfaouri.2003@gmail.com>
2024-08-05 13:21:21 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.InventoryManagement
+
+{
+    public class Product
+    {
+        private int id;
+    private string name=string.Empty;
+        private string? description;
+        private int maxItemsInStock = 0;
+        private UnitType unitType;
+        private int amountInStock=0;
+        private bool isBelowStockThreshold = false;
+    }
+}
